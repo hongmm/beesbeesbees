151b3ee57a297194d5f6e5f646e95ce8472cec8d
Matt Hong <matthew.m.hong@gmail.com>
2020-08-24 12:11:10 -0700

    Assignment Complete

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
 var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  //this.food = Grub.food;
+  //this.eat = Grub.eat;
+  this.job = 'keep on growing';
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
 
